006da9dd0170f90b4b6615fdfa289ca2d00aae91
Harineko0 <harineko0927@gmail.com>
2021-08-24 23:52:10 +0900

    [Add] Add easy golden cross finder

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import csv
 
+import pandas
 from pandas_datareader import data
+import pandas_datareader.data as web
 import numpy
+from datetime import datetime
+
+from config import api_key
 
 
 def simple_moving_average(closes: numpy.ndarray, days: int) -> list[int]:
@@ -29,18 +34,40 @@ def stock_slope(array: numpy.ndarray) -> list[int]:
     return slope
 
 
-if __name__ == '__main__':
-    stock_code: list[str] = []
+def golden_cross(data_frame: pandas.DataFrame) -> bool:
+    return data_frame["5Slope"][0] > 0 and data_frame["5Average"][0] < data_frame["25Average"][0] and \
+           data_frame["5Average"][0] < data_frame["50Average"][0]
+
+
+def get_stock_codes() -> list[str]:
+    codes: list[str] = []
     with open('stocks.csv') as file:
         reader = csv.reader(file)
         for row in reader:
-            stock_code.append(row[1])
-        del stock_code[0]
+            codes.append(row[1])
+        del codes[0]
 
-    print(stock_code)
+    return codes
 
-    df = data.DataReader("7820.JP", "stooq")
-    df.loc[:, "25Average"] = simple_moving_average(df["Close"], 25)
-    df.loc[:, "50Average"] = simple_moving_average(df["Close"], 50)
-    df.loc[:, "5Average"] = simple_moving_average(df["Close"], 5)
-    df.loc[:, "5Slope"] = stock_slope(df["5Average"])
+
+if __name__ == '__main__':
+    stock_codes = get_stock_codes()
+
+    golden_cross_stocks: list[str] = []
+    st = datetime(2021, 6, 10)
+    ed = datetime(2021, 8, 24)
+
+    for code in stock_codes:
+        df = web.DataReader(code + ".JP", "stooq", st, ed, api_key=api_key)
+        if "Close" in df.columns:
+            df.loc[:, "5Average"] = simple_moving_average(df["Close"], 5)
+            df.loc[:, "25Average"] = simple_moving_average(df["Close"], 25)
+            df.loc[:, "50Average"] = simple_moving_average(df["Close"], 50)
+            df.loc[:, "5Slope"] = stock_slope(df["5Average"])
+            print(code)
+            print(df)
+            if golden_cross(df):
+                print(code + ": golden cross")
+                golden_cross_stocks.append(code)
+
+    print(golden_cross_stocks)
