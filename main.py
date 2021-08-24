9eff899d173af523a0a21424abd9fa7d7d347332
Harineko0 <harineko0927@gmail.com>
2021-08-24 20:48:23 +0900

    [Add] Add simple_moving_average

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import numpy
 
 
 def simple_moving_average(closes: numpy.ndarray, days: int) -> list:
-    average = []
-    for i in range(len(closes)):
-        numpy.average()
+    average: list[int] = []
+    length = len(closes)
+    for i in range(length):
+        end_at = i + days - 1
+        if length > end_at:
+            average.append(numpy.average(closes[i: end_at]))
+        else:
+            average.append(0)
+
+    return average
 
 
 if __name__ == '__main__':
     df = data.DataReader("1333.JP", "stooq")
     print(df["Close"].values)
+    print("---------------------------------------------")
+    print(simple_moving_average(df["Close"].values, 5))
