554ff9141d0726abb58e8265d4f704545881792b
Harineko0 <harineko0927@gmail.com>
2021-08-24 23:08:11 +0900

    [Add] Add stocks.csv

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
+import csv
+
 from pandas_datareader import data
 import numpy
 
 
-def simple_moving_average(closes: numpy.ndarray, days: int) -> list:
+def simple_moving_average(closes: numpy.ndarray, days: int) -> list[int]:
     average: list[int] = []
     length = len(closes)
     for i in range(length):
         end_at = i + days - 1
-        if length > end_at:
+        if end_at < length:
             average.append(numpy.average(closes[i: end_at]))
         else:
-            average.append(0)
+            average.append(-1)
 
     return average
 
 
+def stock_slope(array: numpy.ndarray) -> list[int]:
+    slope: list[int] = []
+    length = len(array)
+    for i in range(length):
+        if i + 1 < length and array[i] != -1:
+            slope.append(array[i] - array[i + 1])
+        else:
+            slope.append(-1)
+
+    return slope
+
+
 if __name__ == '__main__':
-    df = data.DataReader("1333.JP", "stooq")
-    print(df["Close"].values)
-    print("---------------------------------------------")
-    print(simple_moving_average(df["Close"].values, 5))
+    stock_code: list[str] = []
+    with open('stocks.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            stock_code.append(row[1])
+        del stock_code[0]
+
+    print(stock_code)
+
+    df = data.DataReader("7820.JP", "stooq")
+    df.loc[:, "25Average"] = simple_moving_average(df["Close"], 25)
+    df.loc[:, "50Average"] = simple_moving_average(df["Close"], 50)
+    df.loc[:, "5Average"] = simple_moving_average(df["Close"], 5)
+    df.loc[:, "5Slope"] = stock_slope(df["5Average"])
